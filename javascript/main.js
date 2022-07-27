69da2708680c26a966a89145244a3e037c9d7adb
Fernando Urzua <fernando.urzua.landa@gmail.com>
2022-07-27 18:56:15 -0500

    prototipo 2.0

---
+++ javascript/main.js
@@ -0,0 +1,35 @@
+function search_product(n) {
+    console.log(n);
+}
+
+function suscribe(n) {
+    console.log(n);
+}
+
+document.getElementById("search_btn").onclick = function () {
+    event.preventDefault();
+    const regex = new RegExp(/^[A-Za-z0-9\s]+$/g);
+    let form_text = document.getElementById("product_name").value;
+    let product_text = regex.test(form_text);
+    if(product_text){
+        search_product(form_text);
+    }
+    else{
+        console.warn("Se ingreso un producto inválido");
+        alert("Favor de solo ingresar letras y números");
+    };
+  };
+
+document.getElementById("email_btn").onclick = function () {
+    event.preventDefault();
+    const regex2 = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    let e_text = document.getElementById("email_text").value;
+    let susc_text = regex2.test(e_text);
+    if(susc_text){
+        suscribe(e_text);
+    }
+    else{
+        console.warn("Se ingreso un correo inválido");
+        alert("Favor de ingresar un correo válido");
+    };
+  };
